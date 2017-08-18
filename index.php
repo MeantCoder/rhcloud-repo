50e09a425eecf23a88cfb74445dbe265929748d7
B. Ugur Demirkan <meantcoder@gmail.com>
2017-08-18 15:40:07 +0300

    DB env check
    Check DB env.

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
+  echo getenv('OPENSHIFT_MYSQL_DB_HOST');
+  echo getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+  echo getenv('OPENSHIFT_MYSQL_DB_HOST');
+  echo getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+  echo getenv('OPENSHIFT_MYSQL_DB_PORT');
 
  ?>
- <title>Hello World</title>
- <!-- Latest compiled and minified CSS -->
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
- <!-- Optional theme -->
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
- <!-- Latest compiled and minified JavaScript -->
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
